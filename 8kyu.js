c8d3ca420c4052aa3f6a8983b4f0b4109881d546
Rendy Artha <rendyartha312@gmail.com>
2021-02-02 22:16:14 +0700

    32 question passed

--- 8kyu.js
+++ 8kyu.js
@@ -351,3 +351,16 @@ function pipeFix(numbers){
   }
   return numbers;
 }
+
+// Drink About
+function peopleWithAgeDrink(old) {
+  if(old < 14) {
+    return ('drink toddy')
+  } else if(old < 18) {
+    return ('drink coke')
+  } else if(old < 21) {
+    return ('drink beer')
+  } else {
+    return ('drink whisky')
+  }
+};
