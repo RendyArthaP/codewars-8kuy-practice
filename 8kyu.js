8d321bdfe71c4aec12de9dbc698d2edf87316fd3
Rendy Artha <rendyartha312@gmail.com>
2021-02-18 23:09:54 +0700

    century from year

--- 8kyu.js
+++ 8kyu.js
@@ -566,3 +566,9 @@ function rentalCarCost(d) {
   if (d >= 3) return d * base - 20
   return d * base
 }
+
+// Century from year
+function century(year) {
+  const result = String(year / 100).split('.')
+  return result.length === 2 ? Number(result[0]) + 1 : Number(result[0])
+}
