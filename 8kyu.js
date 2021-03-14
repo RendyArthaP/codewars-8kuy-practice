c1a482e0cbaaca56314b0668370c72a039d9ce4b
Rendy Artha <rendyartha312@gmail.com>
2021-03-14 23:43:24 +0700

    the feast of many beast

--- 8kyu.js
+++ 8kyu.js
@@ -793,3 +793,12 @@ function sumMix(x){
   }
   return result;
 }
+
+// The feast of many beasts
+function feast(beast, dish) {
+  // Answer 1
+  return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1]
+
+  // Answer 2
+  return beast[0]===dish[0] && beast.slice(-1)===dish.slice(-1);
+}
